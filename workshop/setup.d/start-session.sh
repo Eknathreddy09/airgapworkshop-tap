6023d3c3bca06761afc3fa10e5f4382885d2a22c
Eknathreddy09 <eknath.reddy09@gmail.com>
2023-01-24 17:02:52 +0530

    Update start-session.sh

--- workshop/setup.d/start-session.sh
+++ workshop/setup.d/start-session.sh
@@ -1,1 +1,8 @@
-
+#!/bin/bash
+openjdk version "13.0.1" 2019-10-15
+OpenJDK Runtime Environment (build 13.0.1+9)
+OpenJDK 64-Bit Server VM (build 13.0.1+9, mixed mode, sharing)
+M2_HOME='/opt/apache-maven-3.6.3'
+PATH="$M2_HOME/bin:$PATH"
+export PATH
+#https://www.digitalocean.com/community/tutorials/install-maven-linux-ubuntu
