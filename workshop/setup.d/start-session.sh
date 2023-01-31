b181928a4b3e5d357a8fe32e91a302592214bce5
Eknathreddy09 <eknath.reddy09@gmail.com>
2023-01-31 19:22:23 +0530

    Update start-session.sh

--- workshop/setup.d/start-session.sh
+++ workshop/setup.d/start-session.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-set -x
-set +e
-openjdk version "13.0.1" 2019-10-15
-OpenJDK Runtime Environment (build 13.0.1+9)
-OpenJDK 64-Bit Server VM (build 13.0.1+9, mixed mode, sharing)
+JAVA_HOME='/opt/jdk-13.0.1'
+PATH="$JAVA_HOME/bin:$PATH"
+export PATH
 M2_HOME='/opt/apache-maven-3.6.3'
 PATH="$M2_HOME/bin:$PATH"
 export PATH
